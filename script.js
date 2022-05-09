e1adc1d8f4cd9859237bb71e130b140652df9e08
Maiara Aparecida Da Silva Ferreira <80417551+maiaraf@users.noreply.github.com>
2022-05-09 14:04:59 -0300

    Criado script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let bill = document.querySelector("input#bill");
+let tip = document.querySelectorAll("button.inputTip, button.buttonTip");
+let numberOfPeople = document.querySelector("input#numberOfPeople").value;
+
+function calculate() {
+    console.log(tip.value)
+}
